1b8e917d8bee23c0a985a38c128848640fef933d
Mohammad Firmansyah <76118762+dimasdh842@users.noreply.github.com>
2022-03-14 09:10:10 +0700

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,9 @@
+module golang-hive
+
+go 1.17
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.3.5 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+)
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("Message %s received on topic %s\n", msg.Payload(), msg.Topic())
+}
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	fmt.Println("Connected")
+}
+
+var connectionLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+	fmt.Printf("Connection Lost: %s\n", err.Error())
+}
+
+func main() {
+	var broker = "tcp://test.mosquitto.org:1883"
+	options := mqtt.NewClientOptions()
+	options.AddBroker(broker)
+	options.SetClientID("go_mqtt_example")
+	options.SetDefaultPublishHandler(messagePubHandler)
+	options.OnConnect = connectHandler
+	options.OnConnectionLost = connectionLostHandler
+
+	client := mqtt.NewClient(options)
+	token := client.Connect()
+	if token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	topic := "firmansmoh/v1"
+	token = client.Subscribe(topic, 1, nil)
+	token.Wait()
+	fmt.Printf("Subscribed to topic %s\n", topic)
+
+	num := 10
+	for i := 0; i < num; i-- {
+		token.Wait()
+		time.Sleep(time.Second)
+	}
+
+	// client.Disconnect(500)
+}
